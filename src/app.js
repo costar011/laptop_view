43f71290d09d5a388bb6cf22995e8aead996d741
costar011 <dnaer456@gmail.com>
2021-05-15 14:21:30 +0900

    sql

--- src/app.js
+++ src/app.js
@@ -1,32 +1,40 @@
-import morgan from "morgan";
 import express from "express";
 import helmet from "helmet";
+import morgan from "morgan";
 import path from "path";
 import mysql from "mysql2";
 
+const PORT = 7000;
 const app = express();
-const PORT = 3000;
-// 개발자영어 : req 요청하다, res 응답하다 rander 구성하다,그리다 등등 send 보내다
 
 const conn = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "fourleaf0309",
-  port: "3306",
+  port: "3308",
   database: "academy",
 });
 
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
 app.use(helmet());
-app.use(morgan("dev"));
+app.use(morgan(`dev`));
 
 app.get("/", (req, res) => {
-  // 데이터베이스한테 laptop을 요청하고,
-  // 결과(배열)을 받는다.
-  res.render("home");
+  const sql = `
+    SELECT		latop_no,
+			    model_name,
+			    menufachure,
+			    color,
+			    inch,
+			    cpu,
+			    price,
+      FROM      laptop`;
+  conn.query(sql, (error, rows) => {
+    res.render("home", { laptops: rows });
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server Start PORT`);
+  console.log(`✅ ${PORT} SERVER START`);
 });
