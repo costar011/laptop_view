cd54dc5ba43c91f5365ddbd0b6a7ddbbd935c3f6
costar011 <dnaer456@gmail.com>
2021-05-15 13:54:37 +0900

    app

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ import morgan from "morgan";
 import express from "express";
 import helmet from "helmet";
 import path from "path";
+import mysql from "mysql2";
 
 const app = express();
 const PORT = 3000;
 // 개발자영어 : req 요청하다, res 응답하다 rander 구성하다,그리다 등등 send 보내다
+
+const conn = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "fourleaf0309",
+  port: "3306",
+  database: "academy",
+});
 
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
